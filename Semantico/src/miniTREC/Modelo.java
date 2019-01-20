64620366ce156e0acf8af7cb01c5b21ed665bac2
Rubenbros <rubenbros@hotmail.es>
2019-01-20 01:29:21 +0100

    modelo generado porjena y en el generator falta relacionar con skos

---
+++ Semantico/src/miniTREC/Modelo.java
@@ -0,0 +1,76 @@
+package miniTREC;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.SKOS;
+import org.apache.jena.vocabulary.XSD;
+
+public class Modelo {
+	private static String uri = "http://www.equipo12.com/";
+	public static Model generar() {
+		Model model = ModelFactory.createDefaultModel();
+		Property concept = model.createProperty(uri + "concept");
+		concept.addProperty(RDF.type, RDF.Property);
+		concept.addProperty(RDFS.domain,model.createResource( uri + "trabajo"));
+		concept.addProperty(RDFS.range, SKOS.Concept);
+		Property creator = model.createProperty(uri + "creator");
+		creator.addProperty(RDF.type, RDF.Property);
+		creator.addProperty(RDFS.domain,model.createResource( uri + "trabajo"));
+		creator.addProperty(RDFS.range,model.createResource( uri + "autor"));
+		Property date = model.createProperty(uri + "date");        
+		date.addProperty(RDF.type, RDF.Property);
+		date.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		date.addProperty(RDFS.range, XSD.nonNegativeInteger);
+		Property name = model.createProperty(uri + "name");
+		name.addProperty(RDF.type, RDF.Property);
+		name.addProperty(RDFS.domain,model.createResource( uri + "autor"));
+		name.addProperty(RDFS.range, XSD.xstring);
+		Property apellido1 = model.createProperty(uri + "apellido1");
+		apellido1.addProperty(RDF.type, RDF.Property);
+		apellido1.addProperty(RDFS.domain,model.createResource( uri + "autor"));
+		apellido1.addProperty(RDFS.range, XSD.xstring);
+		Property apellido2 = model.createProperty(uri + "apellido2");
+		apellido2.addProperty(RDF.type, RDF.Property);
+		apellido2.addProperty(RDFS.domain,model.createResource( uri + "autor"));
+		apellido2.addProperty(RDFS.range, XSD.xstring);
+		Property title = model.createProperty(uri + "title");
+		title.addProperty(RDF.type, RDF.Property);
+		title.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		title.addProperty(RDFS.range, XSD.xstring);
+		Property rights = model.createProperty(uri + "rights");
+		rights.addProperty(RDF.type, RDF.Property);
+		rights.addProperty(RDFS.domain,model.createResource( uri + "trabajo"));
+		rights.addProperty(RDFS.range,XSD.xstring);
+		Property type = model.createProperty(uri + "type");
+		type.addProperty(RDF.type, RDF.Property);
+		type.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		type.addProperty(RDFS.range, XSD.xstring);
+		Property format = model.createProperty(uri + "format");
+		format.addProperty(RDF.type, RDF.Property);
+		format.addProperty(RDFS.domain,model.createResource( uri + "trabajo"));
+		format.addProperty(RDFS.range, XSD.xstring);
+		Property language = model.createProperty(uri + "language");
+		language.addProperty(RDF.type, RDF.Property);
+		language.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		language.addProperty(RDFS.range, XSD.xstring);
+		Property publisher = model.createProperty(uri + "publisher");
+		publisher.addProperty(RDF.type, RDF.Property);
+		publisher.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		publisher.addProperty(RDFS.range, XSD.xstring);
+		Property description = model.createProperty(uri + "description");
+		description.addProperty(RDF.type, RDF.Property);
+		description.addProperty(RDFS.domain, model.createResource(uri + "trabajo"));
+		description.addProperty(RDFS.range, XSD.xstring);
+		
+		Resource trabajo = model.createResource(uri+"trabajo");
+		trabajo.addProperty(RDF.type, RDFS.Class);
+		Resource autor = model.createResource(uri + "autor");
+		autor.addProperty(RDF.type, RDFS.Class);
+		model.write(System.out);
+		return model;
+	}
+}
--- Semantico/src/miniTREC/SemanticGenerator.java
+++ Semantico/src/miniTREC/SemanticGenerator.java
@@ -1,13 +1,11 @@
 package miniTREC;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Date;
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -84,49 +82,79 @@ public class SemanticGenerator {
 			aux = st.getSubject().getURI();
 		}
 		File output = new File(rdf);
-		generarRDF(modelo, conceptos, dir, output);
+		File[] file = dir.listFiles();
+		String uri = "http://www.equipo12.com/";
+		Model model = Modelo.generar();
+		for (int i = 0; i < file.length; i++) {
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			org.w3c.dom.Document doc = dBuilder.parse(file[i]);
+			// Sacamos atributos
+			NodeList nList = doc.getElementsByTagName("dc:date");
+			Node nNode = nList.item(0);
+			int anyo = Integer.parseInt(nNode.getTextContent());
+			nList = doc.getElementsByTagName("dc:title");
+			nNode = nList.item(0);
+			String titulo = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:desciption");
+			nNode = nList.item(0);
+			String descripcion = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:publisher");
+			nNode = nList.item(0);
+			String publisher = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:rights");
+			nNode = nList.item(0);
+			String rights = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:type");
+			nNode = nList.item(0);
+			String type = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:language");
+			nNode = nList.item(0);
+			String language = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:format");
+			nNode = nList.item(0);
+			String format = nNode.getTextContent();
+			nList = doc.getElementsByTagName("dc:creator");
+			List<String> autores = new ArrayList<String>();
+			for (int j = 0; j < nList.getLength(); j++)
+				autores.add(nList.item(j).getTextContent());
+			nList = doc.getElementsByTagName("dc:subject");
+			List<String> temas = new ArrayList<String>();
+			for (int j = 0; j < nList.getLength(); j++)
+				temas.add(nList.item(j).getTextContent());
+			//Convertimos los datos en un formato determinado
+			titulo = deAccent(titulo).toLowerCase();
+			descripcion = deAccent(descripcion).toLowerCase();
+			for(String subject : temas)
+				subject = deAccent(subject).toLowerCase();
+			publisher = deAccent(publisher).toLowerCase();
+			//Añadimos los nuevos recursos
+			Resource trabajo = model.createResource(uri+file[i].getName());
+			trabajo.addLiteral(model.getProperty(uri+"title"), titulo);
+			trabajo.addLiteral(model.getProperty(uri+"description"), descripcion);
+			trabajo.addLiteral(model.getProperty(uri+"rights"), rights);
+			trabajo.addLiteral(model.getProperty(uri+"format"), format);
+			trabajo.addLiteral(model.getProperty(uri+"language"), language);
+			trabajo.addLiteral(model.getProperty(uri+"publisher"), publisher);
+			trabajo.addLiteral(model.getProperty(uri+"type"), type);
+			trabajo.addLiteral(model.getProperty(uri+"date"),anyo);
+			for(String person : autores) {
+				String[] aux1 = person.split(" ");
+				String name =aux1[2];
+				String apellido1 = aux1[0];
+				String apellido2 = aux1[1].replaceAll(",","");
+				Resource autor = model.createResource(uri+name+"_"+apellido1+"_"+apellido2);
+				autor.addLiteral(model.getProperty(uri+"name"), name);
+				autor.addLiteral(model.getProperty(uri+"apellido1"), apellido1);
+				autor.addLiteral(model.getProperty(uri+"apellido2"), apellido2);
+				trabajo.addLiteral(model.getProperty(uri+"creator"), autor);
+			}
+		}
 	}
 
-	private static void generarRDF(Model modelo, ArrayList<String> conceptos, File dir, File output) throws SAXException, IOException, ParserConfigurationException {
-		File[] file = dir.listFiles();
-		FileInputStream fis = null;
-		for (int i = 0; i < file.length; i++) {
-				fis = new FileInputStream(file[i]);
-				String title=null;
-				DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-				DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-				org.w3c.dom.Document doc = dBuilder.parse(file[i]);
-				//Sacamos atributos
-				NodeList nList = doc.getElementsByTagName("dc:date");
-				Node nNode = nList.item(0);
-				int anyo = Integer.parseInt(nNode.getTextContent());
-				nList = doc.getElementsByTagName("dc:title");
-				nNode = nList.item(0);
-				String titulo = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:publisher");
-				nNode = nList.item(0);
-				String publisher = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:rights");
-				nNode = nList.item(0);
-				String rights = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:type");
-				nNode = nList.item(0);
-				String type = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:language");
-				nNode = nList.item(0);
-				String language = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:format");
-				nNode = nList.item(0);
-				String format = nNode.getTextContent();
-				nList = doc.getElementsByTagName("dc:creator");
-				List<String> autores = new ArrayList<String>();
-				for (int j = 0; j < nList.getLength(); j++)
-					autores.add(nList.item(j).getTextContent());
-				nList = doc.getElementsByTagName("dc:subject");
-				List<String> temas = new ArrayList<String>();
-				for (int j = 0; j < nList.getLength(); j++)
-					temas.add(nList.item(j).getTextContent());
-				
-		}
+	private static String deAccent(String str) {
+		String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
+		Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+		return pattern.matcher(nfdNormalizedString).replaceAll("");
 	}
 }
